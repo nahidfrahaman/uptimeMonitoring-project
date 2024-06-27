fd2feb553f30f4363ea49bf4ab85214b047f2f44
NahidFRahaman <nahi.frahaman@gmail.com>
2024-06-27 08:29:35 +0600

    ready for handle request server

--- index.js
+++ index.js
@@ -8,6 +8,9 @@ inspired by : sumit shaha
 
 // dependencis 
 const http = require('http');
+const url = require('url');
+const {StringDecoder} = require('string_decoder') ;
+
 
 // app object - module scaffolding 
 const app = {};
@@ -26,10 +29,37 @@ app.createServer  = () => {
     })
 }
 
-
 // handle req res
 app.handleRequest = (req, res) => {
-    res.end('hello wolrd')
+    // requrest handling 
+    // get the url and parse it 
+    const parsedUrl = url.parse(req.url, true);
+    const path = parsedUrl.pathname;
+    const tirmedPath = path.replace(/^\/|\/+$/g,'')
+    console.log(tirmedPath);
+    
+    // get method   // get method     // get header 
+    const method = req.method.toLowerCase();
+    const queryObject = parsedUrl.query ;
+    const headersObject = req.headers;
+  
+    // req body handle 
+    const decoder = new StringDecoder('utf-8')
+    let realdata = "";
+
+
+    req.on('data', (buffer) => {
+        realdata += decoder.write(buffer)
+    })
+
+    req.on('end', () => {
+        realdata += decoder.end();
+        console.log(realdata);
+        res.end('hello programer')
+    })
+
+    // response handle 
+   
 }
 
 // run the server function 
