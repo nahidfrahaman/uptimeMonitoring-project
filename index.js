2ceb035ae12e1fcd62df83ce130db47cf44a2b5a
NahidFRahaman <nahi.frahaman@gmail.com>
2024-06-27 07:08:56 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,36 @@
+/*
+title : Uptime Monitoring Application 
+Description : A restful API to monitor up or down user define links
+Author : nahid 
+Date : 26-06-24
+inspired by : sumit shaha
+*/
+
+// dependencis 
+const http = require('http');
+
+// app object - module scaffolding 
+const app = {};
+
+//configaration 
+app.config = {
+    port : 3000
+}
+
+// create Server 
+app.createServer  = () => {
+    const server = http.createServer(app.handleRequest);
+    server.listen(app.config.port, () => {
+        console.log('listening port in ' + app.config.port);
+        
+    })
+}
+
+
+// handle req res
+app.handleRequest = (req, res) => {
+    res.end('hello wolrd')
+}
+
+// run the server function 
+app.createServer();
